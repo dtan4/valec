d2f1b80ac99527f7f3400d7a82d633bcead37c7e
Daisuke Fujita <dtanshi45@gmail.com>
2016-11-24 16:37:34 +0900

    List key-values stored in yaml

--- cmd/list.go
+++ cmd/list.go
@@ -2,14 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/dtan4/valec/aws"
+	"github.com/dtan4/valec/lib"
 	"github.com/spf13/cobra"
-)
-
-var (
-	key  string
-	text string
+	"gopkg.in/yaml.v2"
 )
 
 // listCmd represents the list command
@@ -23,12 +24,35 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		plaintText, err := aws.KMS().DecryptBase64(key, text)
+		if len(args) != 1 {
+			return fmt.Errorf("Please specify config file.")
+		}
+		filename := args[0]
+
+		var configs []*lib.Config
+
+		body, err := ioutil.ReadFile(filename)
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(plaintText)
+		if err := yaml.Unmarshal(body, &configs); err != nil {
+			return err
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
+		fmt.Fprintln(w, strings.Join([]string{"KEY", "VALUE"}, "\t"))
+
+		for _, config := range configs {
+			plainValue, err := aws.KMS().DecryptBase64(config.Key, config.Value)
+			if err != nil {
+				return err
+			}
+
+			fmt.Fprintln(w, strings.Join([]string{config.Key, plainValue}, "\t"))
+		}
+
+		w.Flush()
 
 		return nil
 	},
@@ -36,7 +60,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	RootCmd.AddCommand(listCmd)
-
-	listCmd.Flags().StringVarP(&key, "key", "k", "", "Key")
-	listCmd.Flags().StringVarP(&text, "text", "t", "", "Text to decrypt")
 }
---
+++ lib/types.go
@@ -0,0 +1,7 @@
+package lib
+
+// Config represents key=value pair
+type Config struct {
+	Key   string `yaml:"key"`
+	Value string `yaml:"value"`
+}
