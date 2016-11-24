2b879e62864583dc67ae5745adb485b2ba604bea
Daisuke Fujita <dtanshi45@gmail.com>
2016-11-24 17:10:06 +0900

    Remove unneeded error output

--- cmd/root.go
+++ cmd/root.go
@@ -20,8 +20,10 @@ var (
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "valec",
-	Short: "A brief description of your application",
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	Use:           "valec",
+	Short:         "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
