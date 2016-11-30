84e9d1052e214f0c4bf68f668541f00483ddcb49
Daisuke Fujita <dtanshi45@gmail.com>
2016-11-30 17:31:29 +0900

    Implement version command

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dtan4/valec/version"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version.String())
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
