012219810373a1eabbb7e998f4bb2e9b62e35f38
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-25 11:46:51 +0900

    Implement get command

---
+++ cmd/get.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dtan4/valec/aws"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get NAMESPACE KEY",
+	Short: "Get secret",
+	RunE:  doGet,
+}
+
+func doGet(cmd *cobra.Command, args []string) error {
+	if len(args) != 2 {
+		return errors.New("Please specify both namespace and key.")
+	}
+	namespace, key := args[0], args[1]
+
+	secret, err := aws.DynamoDB.Get(rootOpts.tableName, namespace, key)
+	if err != nil {
+		return errors.Wrap(err, "Failed to retrieve secret.")
+	}
+
+	plainValue, err := aws.KMS.DecryptBase64(secret.Key, secret.Value)
+	if err != nil {
+		return errors.Wrap(err, "Failed to decrypt secret.")
+	}
+
+	fmt.Printf(plainValue)
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(getCmd)
+}
